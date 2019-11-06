10ac8ffd8a9414769119c51f3a02b01c84b9bbda
hudo9921 <57452786+hudo9921@users.noreply.github.com>
2019-11-06 15:10:04 -0300

    TelaCheia

--- RayTrab/VSCode/main.c
+++ RayTrab/VSCode/main.c
@@ -1,20 +1,21 @@
 #include "raylib.h"
-#include <stdio.h>
 
 int main() 
 {
-    printf("\nTESTE\n");
-    int height = 600;
-    int width = 800;
+    int Tela_Largura=GetScreenWidth();
+    int Tela_Altura=GetScreenHeight();
+    SetTargetFPS(60);
+    
 
-    InitWindow(width,height,"Cena1");
+    InitWindow(Tela_Largura,Tela_Altura,"Cena1");
     while (!WindowShouldClose())
     {
+        
         BeginDrawing();
-                ClearBackground(WHITE);
-                DrawRectangle(width/8,height/8,40,30,BLUE);
-                DrawCircle((width/2),(height/2),50,RED);
-        EndDrawing(); 
-    };
-    return 0;
+            ClearBackground(WHITE);
+            DrawRectangle(GetScreenWidth()/2,GetScreenHeight()/2,40,50,RED);
+        EndDrawing();    
+    }
+    CloseWindow();
+    
 }
