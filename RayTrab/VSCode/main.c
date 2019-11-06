50c20d58468427486c990bcc90d8b954ddf1d347
hudo9921 <57452786+hudo9921@users.noreply.github.com>
2019-11-06 15:31:05 -0300

    PrimeiraTentativaMapa
    Tentando criar o map(precisa ajeitar o tamanho e pos, ta troll)

--- RayTrab/VSCode/main.c
+++ RayTrab/VSCode/main.c
@@ -2,18 +2,35 @@
 
 int main() 
 {
-    int Tela_Largura=GetScreenWidth();
-    int Tela_Altura=GetScreenHeight();
+    int Tela_Largura=1200;
+    int Tela_Altura=800;
+    int Mapa1[24][16];
+
+    for (int i = 0; i < 16; i++)
+    {
+        for (int j = 0; j <24 ; j++)
+        {
+            Mapa1[i][j]=0;
+        }
+        
+    }
+
     SetTargetFPS(60);
     
-
     InitWindow(Tela_Largura,Tela_Altura,"Cena1");
     while (!WindowShouldClose())
     {
         
         BeginDrawing();
             ClearBackground(WHITE);
-            DrawRectangle(GetScreenWidth()/2,GetScreenHeight()/2,40,50,RED);
+            for (int i = 0; i < 16; i++)
+                {
+                    for (int j = 0; j <24 ; j++)
+                        {
+                            DrawRectangle(120+(j*40),80+(i*40),50,50,BLACK);
+                        }
+                    
+                }
         EndDrawing();    
     }
     CloseWindow();
