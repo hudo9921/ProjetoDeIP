e134df5c5cce84a03dab6fc9bded10dd986d56e6
Samuelvidal99 <57455065+Samuelvidal99@users.noreply.github.com>
2019-11-06 22:42:31 -0300

    "Personagem" e Fps
    representação de personagem como bloco e que se move adicionado e contador de fps

--- RayTrab/VSCode/main.c
+++ RayTrab/VSCode/main.c
@@ -5,6 +5,7 @@ int main()
     int Tela_Largura=1200;
     int Tela_Altura=800;
     int Mapa1[24][16];
+    Vector2 movimento_personagem = {(float)Tela_Altura/2,(float)Tela_Largura/2};
 
     for (int i = 0; i < 16; i++)
     {
@@ -20,17 +21,22 @@ int main()
     InitWindow(Tela_Largura,Tela_Altura,"Cena1");
     while (!WindowShouldClose())
     {
+        if (IsKeyDown(KEY_RIGHT)) movimento_personagem.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) movimento_personagem.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) movimento_personagem.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) movimento_personagem.y += 2.0f;
         
         BeginDrawing();
             ClearBackground(WHITE);
             for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j <24 ; j++)
                 {
-                    for (int j = 0; j <24 ; j++)
-                        {
-                            DrawRectangle(120+(j*40),80+(i*40),50,50,BLACK);
-                        }
-                    
-                }
+                    DrawRectangle(120+(j*40),80+(i*40),50,50,BLACK);
+                }  
+            }
+            DrawFPS(50,50);
+            DrawRectangle(movimento_personagem.x,movimento_personagem.y,50,50,RED);
         EndDrawing();    
     }
     CloseWindow();
