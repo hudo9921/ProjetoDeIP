4b22da1a1245b96eafdebf86992b8e8394061d34
Samuelvidal99 <57455065+Samuelvidal99@users.noreply.github.com>
2019-11-17 17:07:25 -0300

    Movimentação do personagem organizada

--- RayTrab/VSCode/main.c
+++ RayTrab/VSCode/main.c
@@ -5,26 +5,190 @@
 const int largura = 1280;
 const int altura = 720;
 
-void textura()
+typedef struct 
 {
+    int x;
+    int contador;
+    int animar;
+    float frame_largura;
+    int max_frames;
+    float timer;
+    int frame;
+    Vector2 posicao_quadrado;
+    Texture2D char_walk
+    
+}Jogador;
 
+void personagem_movimentacao( Jogador* jogador )
+{
+    if( (jogador->posicao_quadrado.x + 2.0f) <= 60+(24*40) && (jogador->posicao_quadrado.x - 2.0f) >= 60+(1*40) &&  (jogador->posicao_quadrado.y + 2.0f) <= (16*40) - 20 && (jogador->posicao_quadrado.y - 2.0f) >= (1*40) - 20 )
+    {
+        if (IsKeyDown(KEY_UP))
+        {
+            jogador->posicao_quadrado.y -= 2.0f+4;
+            jogador->contador += 1;
+            jogador->animar++; 
+        } 
+        if (IsKeyDown(KEY_DOWN))
+        {
+            jogador->posicao_quadrado.y += 2.0f+4;
+            jogador->contador += 2;
+            jogador->animar++;
+        } 
+        if (IsKeyDown(KEY_RIGHT))
+        {
+            jogador->posicao_quadrado.x += 2.0f+4;
+            jogador->contador += 3;
+            jogador->animar++;
+        } 
+        if (IsKeyDown(KEY_LEFT))
+        {
+            jogador->posicao_quadrado.x -= 2.0f+4;
+            jogador->contador += 4;
+            jogador->animar++;
+        } 
+        if(IsKeyDown(KEY_UP) && IsKeyDown(KEY_RIGHT))
+        {
+            jogador->posicao_quadrado.y -= 1.0f+2;
+            jogador->contador += 1;
+            jogador->animar++; 
+            jogador->posicao_quadrado.x += 1.0f+2;
+        }
+        if(IsKeyDown(KEY_UP) && IsKeyDown(KEY_LEFT))
+        {
+            jogador->posicao_quadrado.y -= 1.0f+2;
+            jogador->contador += 1;
+            jogador->animar++; 
+            jogador->posicao_quadrado.x -= 1.0f+2;
+        }
+        if(IsKeyDown(KEY_DOWN) && IsKeyDown(KEY_RIGHT))
+        {
+            jogador->posicao_quadrado.y += 1.0f+2;
+            jogador->contador += 2;
+            jogador->animar++;
+            jogador->posicao_quadrado.x += 1.0f+2;
+        }
+        if(IsKeyDown(KEY_DOWN) && IsKeyDown(KEY_LEFT))
+        {
+            jogador->posicao_quadrado.y += 1.0f+2;
+            jogador->contador += 2;
+            jogador->animar++;
+            jogador->posicao_quadrado.x -= 1.0f+2;
+        }
+        if(IsKeyReleased(KEY_UP))
+        {
+            UnloadTexture(jogador->char_walk);
+            jogador->char_walk = LoadTexture("img/prisioneiro_pose02.png");
+            jogador->animar = 0;
+        }
+        if(IsKeyReleased(KEY_DOWN))
+        {
+            UnloadTexture(jogador->char_walk);
+            jogador->char_walk = LoadTexture("img/prisioneiro_pose01.png");
+            jogador->animar = 0;
+        }
+        if(IsKeyReleased(KEY_RIGHT))
+        {
+            UnloadTexture(jogador->char_walk);
+            jogador->char_walk = LoadTexture("img/prisioneiro_pose03.png");
+            jogador->animar = 0;
+        }
+        if(IsKeyReleased(KEY_LEFT))
+        {
+            UnloadTexture(jogador->char_walk);
+            jogador->char_walk = LoadTexture("img/prisioneiro_pose04.png");
+            jogador->animar = 0;
+        }
+    }
+    else if ( (jogador->posicao_quadrado.x + 2.0f) >= 60+(24*40) )
+    {
+        jogador->posicao_quadrado.x = jogador->posicao_quadrado.x - 0.5;
+    }
+    else if ( (jogador->posicao_quadrado.y + 2.0f) >= (16*40) - 20 )
+    {
+        jogador->posicao_quadrado.y = jogador->posicao_quadrado.y - 0.5;
+    }
+    else if ( (jogador->posicao_quadrado.x - 2.0f) <= 60+(1*40) )
+    {
+        jogador->posicao_quadrado.x = jogador->posicao_quadrado.x + 0.5;         
+    }
+    else if( (jogador->posicao_quadrado.y - 2.0f) <= (1*40) - 20 )
+    {
+        jogador->posicao_quadrado.y = jogador->posicao_quadrado.y + 0.5;
+    }
+}
+
+void draw_jogador(Jogador* jogador)
+{
+    if(jogador->contador == 1)
+    {
+        UnloadTexture(jogador->char_walk);
+        jogador->char_walk = LoadTexture("img/prisioneiro_animacao02.png");
+        jogador->contador = 0;
+    }
+    if(jogador->contador == 2)
+    {
+        UnloadTexture(jogador->char_walk);
+        jogador->char_walk = LoadTexture("img/prisioneiro_animacao01.png");
+        jogador->contador = 0;
+    }
+    if(jogador->contador == 3)
+    {
+        UnloadTexture(jogador->char_walk);
+        jogador->char_walk = LoadTexture("img/prisioneiro_animacao03.png");
+        jogador->contador = 0;
+    }
+    if(jogador->contador == 4)
+    {
+        UnloadTexture(jogador->char_walk);
+        jogador->char_walk = LoadTexture("img/prisioneiro_animacao04.png");
+        jogador->contador = 0;
+    }
+    if( jogador->animar != 0 )
+    {
+        jogador->timer += GetFrameTime();
+        if( jogador->timer >= 0.2f )
+        {
+            jogador->timer = 0.0f;
+            jogador->frame++;
+        }
+        jogador->frame_largura = (float)( jogador->char_walk.width/2 );
+        jogador->max_frames = (int)( jogador->char_walk.width / (int)jogador->frame_largura);
+        jogador->frame = jogador->frame % jogador->max_frames;
+        jogador->x = (jogador->frame_largura*jogador->frame);
+    }
+    else
+    {
+        jogador->frame_largura = (float)(jogador->char_walk.width);
+        jogador->max_frames = 0;
+        jogador->frame = 1;
+        jogador->x = 0;
+        jogador->animar = 0;
+    }   
+    Rectangle rectangle_walk = { jogador->x, 0, jogador->frame_largura,(float) jogador->char_walk.height };
+    Vector2 vector_walk = { jogador->posicao_quadrado.x, jogador->posicao_quadrado.y };
+    DrawTextureRec(
+        jogador->char_walk,
+        rectangle_walk,
+        vector_walk,
+        RAYWHITE);;
 }
 
 int main() 
 {
-    int x1 = 0;
-    int contador = 0, animar = 0;
+    Jogador jogador;
+    jogador.x = 0;
+    jogador.animar = 0;
+    jogador.contador = 0;
+    
     printf("\nXDXDXD\n");
     InitWindow(largura,altura,"teste");
     SetTargetFPS(60);
     Texture2D chao = LoadTexture("img/chao3.png");
-
-    //personagem animação andar1
-    Texture2D char_walk1 = LoadTexture("img/prisioneiro_pose02.png");
-    float frame_largura1 = (float)(char_walk1.width);
-    int max_frames1 = 0;
-    float timer1 = 0.0f;
-    int frame1 = 0;
+   
+    jogador.posicao_quadrado.x = (float)altura/2+50;
+    jogador.posicao_quadrado.y = (float)largura/2-50;
+    jogador.char_walk = LoadTexture("img/prisioneiro_pose02.png");
 
     int Mapa1[24][16];
     for (int i = 0; i < 16; i++)
@@ -32,109 +196,13 @@ int main()
         for (int j = 0; j <24 ; j++)
         {
             Mapa1[i][j]=0;
-        }
-        
-    }
-
-    Vector2 posicao_quadrado = { (float)altura/2+50 , (float)largura/2-50 };
+        }   
+    } 
 
     while (!WindowShouldClose())
     {
-        if( (posicao_quadrado.x + 2.0f) <= 60+(24*40) && (posicao_quadrado.x - 2.0f) >= 60+(1*40) &&  (posicao_quadrado.y + 2.0f) <= (16*40) - 20 && (posicao_quadrado.y - 2.0f) >= (1*40) - 20 )
-        {
-            if (IsKeyDown(KEY_UP))
-            {
-               posicao_quadrado.y -= 2.0f+4;
-               contador += 1;
-               animar++; 
-            } 
-            if (IsKeyDown(KEY_DOWN))
-            {
-                posicao_quadrado.y += 2.0f+4;
-                contador += 2;
-                animar++;
-            } 
-            if (IsKeyDown(KEY_RIGHT))
-            {
-                posicao_quadrado.x += 2.0f+4;
-                contador += 3;
-                animar++;
-            } 
-            if (IsKeyDown(KEY_LEFT))
-            {
-                posicao_quadrado.x -= 2.0f+4;
-                contador += 4;
-                animar++;
-            } 
-            if(IsKeyDown(KEY_UP) && IsKeyDown(KEY_RIGHT))
-            {
-                posicao_quadrado.y -= 1.0f+2;
-                contador += 1;
-                animar++; 
-                posicao_quadrado.x += 1.0f+2;
-            }
-            if(IsKeyDown(KEY_UP) && IsKeyDown(KEY_LEFT))
-            {
-                posicao_quadrado.y -= 1.0f+2;
-                contador += 1;
-                animar++; 
-                posicao_quadrado.x -= 1.0f+2;
-            }
-            if(IsKeyDown(KEY_DOWN) && IsKeyDown(KEY_RIGHT))
-            {
-                posicao_quadrado.y += 1.0f+2;
-                contador += 2;
-                animar++;
-                posicao_quadrado.x += 1.0f+2;
-            }
-            if(IsKeyDown(KEY_DOWN) && IsKeyDown(KEY_LEFT))
-            {
-                posicao_quadrado.y += 1.0f+2;
-                contador += 2;
-                animar++;
-                posicao_quadrado.x -= 1.0f+2;
-            }
-            if(IsKeyReleased(KEY_UP))
-            {
-                UnloadTexture(char_walk1);
-                char_walk1 = LoadTexture("img/prisioneiro_pose02.png");
-                animar = 0;
-            }
-            if(IsKeyReleased(KEY_DOWN))
-            {
-                UnloadTexture(char_walk1);
-                char_walk1 = LoadTexture("img/prisioneiro_pose01.png");
-                animar = 0;
-            }
-            if(IsKeyReleased(KEY_RIGHT))
-            {
-                UnloadTexture(char_walk1);
-                char_walk1 = LoadTexture("img/prisioneiro_pose03.png");
-                animar = 0;
-            }
-            if(IsKeyReleased(KEY_LEFT))
-            {
-                UnloadTexture(char_walk1);
-                char_walk1 = LoadTexture("img/prisioneiro_pose04.png");
-                animar = 0;
-            }
-        }
-        else if ( (posicao_quadrado.x + 2.0f) >= 60+(24*40) )
-        {
-            posicao_quadrado.x = posicao_quadrado.x - 0.5;
-        }
-        else if ( (posicao_quadrado.y + 2.0f) >= (16*40) - 20 )
-        {
-            posicao_quadrado.y = posicao_quadrado.y - 0.5;
-        }
-        else if ( (posicao_quadrado.x - 2.0f) <= 60+(1*40) )
-        {
-            posicao_quadrado.x = posicao_quadrado.x + 0.5;         
-        }
-        else if( (posicao_quadrado.y - 2.0f) <= (1*40) - 20 )
-        {
-            posicao_quadrado.y = posicao_quadrado.y + 0.5;
-        }
+        personagem_movimentacao(&jogador);
+        
         BeginDrawing();
 
             ClearBackground(WHITE);
@@ -146,59 +214,11 @@ int main()
                 }  
             }
             DrawFPS(30,30);
-            if(contador == 1)
-            {
-                UnloadTexture(char_walk1);
-                char_walk1 = LoadTexture("img/prisioneiro_animacao02.png");
-                contador = 0;
-            }
-            if(contador == 2)
-            {
-                UnloadTexture(char_walk1);
-                char_walk1 = LoadTexture("img/prisioneiro_animacao01.png");
-                contador = 0;
-            }
-            if(contador == 3)
-            {
-                UnloadTexture(char_walk1);
-                char_walk1 = LoadTexture("img/prisioneiro_animacao03.png");
-                contador = 0;
-            }
-            if(contador == 4)
-            {
-                UnloadTexture(char_walk1);
-                char_walk1 = LoadTexture("img/prisioneiro_animacao04.png");
-                contador = 0;
-            }
-            if( animar != 0 )
-            {
-                timer1 += GetFrameTime();
-                if( timer1 >= 0.4f )
-                {
-                    timer1 = 0.0f;
-                    frame1++;
-                }
-                frame_largura1 = (float)( char_walk1.width/2 );
-                max_frames1 = (int)( char_walk1.width / (int)frame_largura1);
-                frame1 = frame1 % max_frames1;
-                x1 = (frame_largura1*frame1);
-            }
-            else
-            {
-                frame_largura1 = (float)(char_walk1.width);
-                max_frames1 = 0;
-                frame1 = 1;
-                x1 = 0;
-                animar = 0;
-            }   
-            Rectangle rectangle_walk1 = { x1, 0, frame_largura1,(float) char_walk1.height };
-            Vector2 vector_walk1 = { posicao_quadrado.x, posicao_quadrado.y };
-            DrawTextureRec(
-                char_walk1,
-                rectangle_walk1,
-                vector_walk1,
-                RAYWHITE);
-            contador = 0;
+
+            draw_jogador(&jogador);
+            
+            jogador.contador = 0;
+
         EndDrawing();
     }
 
